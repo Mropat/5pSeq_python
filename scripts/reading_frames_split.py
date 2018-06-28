ec9991250639b62c3e15ab8c8180243b0221b68c
Maria <mariaropat@gmail.com>
2018-06-28 11:59:23 +0200

    Outliers eliminated from metagene

--- scripts/reading_frames_split.py
+++ scripts/reading_frames_split.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 
 def extend_gtf_frame(pickle):
     gtf_coords_file = list(dill.load(open(pickle, "rb")))
+    allowed_ids = set(allowed_transcript_ids(gene_set_path))
     for transcript in gtf_coords_file:
-        span = transcript.spanning_segment
-        new_region = GenomicSegment(
-            span.chrom, span.start - offset, span.start, span.strand)
-        new_region_2 = GenomicSegment(
-            span.chrom, span.end, span.end + offset, span.strand)
-        transcript.add_segments(new_region, new_region_2)
-    return gtf_coords_file
+        if transcript.get_name() in allowed_ids:
+            span = transcript.spanning_segment
+            new_region = GenomicSegment(
+                span.chrom, span.start - offset, span.start, span.strand)
+            new_region_2 = GenomicSegment(
+                span.chrom, span.end, span.end + offset, span.strand)
+            transcript.add_segments(new_region, new_region_2)
+            yield transcript
 
 
 def match_coords_to_seq(filename):
@@ -40,59 +42,45 @@ def allowed_transcript_ids(filename):
 
 
 def fetch_vectors(filename):
-    fasta_dict = match_coords_to_seq(genome_fasta_path)
-    allowed_ids = set(allowed_transcript_ids(gene_set_path))
+#    fasta_dict = match_coords_to_seq(genome_fasta_path)
     alignments = BAMGenomeArray(filename, mapping=FivePrimeMapFactory())
 
     count_vectors = []
     filler_array = np.zeros(300, dtype=int)
     for transcript in extend_gtf_frame(gtf_assembly_pickle):
-        transcript_length = transcript.get_length()
-        if transcript.get_name() in allowed_ids:
-            if transcript_length in range(400, 800):
-                value_array = transcript.get_counts(alignments)
-                value_array = np.concatenate((value_array, filler_array))
-                count_vectors.append(value_array[:600])
+        if transcript.get_length() > 400:            
+            value_array = transcript.get_counts(alignments)
+            value_array = np.concatenate((value_array, filler_array))
+            count_vectors.append(value_array[50:650])
 
     vector_array = np.vstack(count_vectors)
+
+    global_mean = (np.mean(vector_array[vector_array > 1]))
+    global_stdev = (np.std(vector_array[vector_array > 1]))
+    outlier = global_mean + global_stdev * 3
+    vector_array[vector_array > outlier] = outlier
+
     metagene = vector_array.sum(axis=0)
     metagene_stack = np.reshape(metagene, (200, 3))
     metagene_stack = np.hsplit(metagene_stack, 3)
 
     frames = []
     for arr in metagene_stack:
-        frame_vector = np.reshape(arr.T, (1, -1))
-        frame_vector = np.concatenate(
-            (np.zeros(2), frame_vector[0], np.zeros(2)))
-
-        window_iter = (np.sum(frame_vector[i-2:i+2])
-                       for i in range(len(frame_vector[2:-3])))
-        sliding_vector = np.fromiter(window_iter, dtype=int)
+        frame_vector = np.concatenate((np.zeros(2), arr.T[0], np.zeros(2)))
+        window_iter = (np.sum(frame_vector[i:i+5]) for i in range(len(frame_vector[2:-3])))
+        sliding_vector = np.fromiter(window_iter, dtype=float)
         frames.append(sliding_vector)
-
     return frames
 
 
-def scale_labels():
-    xlabels = []
-    for x in range(-48, 49):
-        if x % 10 == 0:
-            xlabels.append(x)
-        else:
-            xlabels.append(" ")
-    return xlabels
-
-
-def plot_results_coding(frames):
-    labels = scale_labels()
-
+def plot_results_coding():
+    frames = fetch_vectors(bam_file_path)
     frame_0, frame_1, frame_2 = frames[0], frames[1], frames[2]
 
     plt.grid(True, alpha=0.3)
-    plt.plot(frame_0, linewidth=0.5, color="red")
-    plt.plot(frame_1, linewidth=0.5, color="blue")
-    plt.plot(frame_2, linewidth=0.5, color="green")
-
+    plt.plot(frame_0, linewidth=0.5, color="blue", label="Frame 0" )
+    plt.plot(frame_1, linewidth=1, color="red", label ="Frame +1 (main)")
+    plt.plot(frame_2, linewidth=0.5, color="green", label="Frame -1")
     plt.savefig("frames_sliding.pdf")
     plt.close()
 
@@ -103,6 +91,4 @@ gtf_assembly_pickle = "gtf_assembled.sav"
 bam_file_path = "/home/maria/Documents/pelechanolab/data/samples/20160909YP_08-39666715/cutadapt/umitools/star/BY4741-t5-1_S8_L002_R1_001.fastqAligned.sortedByCoord.out.bam"
 offset = 50
 
-
-frames = fetch_vectors(bam_file_path)
-plot_results_coding(frames)
+plot_results_coding()
